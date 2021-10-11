784d19fd1a4cb46672be49b4d4926c19836001b4
xiaosu <2257346739@qq.com>
2021-10-11 15:29:50 +0800

    Spring自动注入byType

---
+++ spring-di-xml/src/main/java/com/company/di05/PrimarySchool.java
@@ -0,0 +1,12 @@
+package com.company.di05;
+
+/**
+ * @author 苏东坡
+ * @version 1.0
+ * @ClassName PrimarySchool
+ * @company 公司
+ * @Description 小学
+ * @createTime 2021年10月11日 15:22:22
+ */
+public class PrimarySchool extends  School{
+}
