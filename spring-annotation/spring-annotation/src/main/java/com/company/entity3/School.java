7bb776a4eb2354eb25b67a352c2547ab6de1ce79
xiaosu <2257346739@qq.com>
2021-10-12 19:52:46 +0800

    Spring引用类型注解：@Autowired

---
+++ spring-annotation/spring-annotation/src/main/java/com/company/entity3/School.java
@@ -0,0 +1,39 @@
+package com.company.entity3;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author 苏东坡
+ * @version 1.0
+ * @ClassName School
+ * @company 公司
+ * @Description TODO
+ * @createTime 2021年10月10日 14:45:45
+ */
+@Component("school")
+public class School {
+
+    @Value("北京大学")
+    private String name;
+
+    @Value("中关村")
+    private String address;
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "School{" +
+                "name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
