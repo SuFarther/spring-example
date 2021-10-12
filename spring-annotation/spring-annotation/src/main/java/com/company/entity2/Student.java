ad62f8e5e5e181cdba9c221a288b591b06ea1c4c
xiaosu <2257346739@qq.com>
2021-10-12 19:34:29 +0800

    spring中的三个注解，@Repository，@Service，@Controller和@Component功能一致,但是@Component什么时候用呢,当不使用那三项就用

---
+++ spring-annotation/spring-annotation/src/main/java/com/company/entity2/Student.java
@@ -0,0 +1,56 @@
+package com.company.entity2;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+/**
+ * @version 1.0
+ * @ClassName Student
+ * @company 公司
+ * @Description TODO
+ * @createTime 2021年10月12日 19:08:08
+ */
+@Component("student2")
+public class Student {
+
+//    @Value(value = "张飞")
+
+    @Value("张飞")
+    private String name;
+
+//    @Value(value = "20")
+
+    @Value("20")
+    private Integer age;
+
+    public Student(){
+
+    }
+
+    public Student(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Value("20")
+    public void setAge(Integer age) {
+        System.out.println("setAge" +age);
+        this.age = age;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
